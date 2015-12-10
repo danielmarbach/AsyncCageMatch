451fe0102a5fe8bdcac19297aac74709d16b7eb1
danielmarbach <daniel.marbach@openplace.net>
2015-12-10 21:23:18 +0100

    Primitive async only

---
+++ AsyncOnly/Program.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace AsyncOnly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            try
+            {
+                DoIt().GetAwaiter().GetResult();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+            stopWatch.Stop();
+            Console.WriteLine();
+            Console.WriteLine(stopWatch.Elapsed);
+            Console.ReadLine();
+        }
+
+        private static Task DoIt()
+        {
+            var behaviors = new List<IBehavior>
+            {
+                new DelayBehavior1(),
+                new DelayBehavior2(),
+                new DelayBehavior3(),
+                new DelayBehavior4(),
+                new DelayInUsingBehavior1(),
+                new PassThroughBehavior1(),
+                new DelayTwiceBehavior1(),
+                new DelayTwiceBehavior2(),
+                new DelayTwiceBehavior3(),
+                new DelayTwiceBehavior4(),
+                new DelayInUsingBehavior2(),
+                new PassThroughBehavior2(),
+                new DelayBehavior5(),
+                new DelayBehavior6(),
+                new DelayBehavior7(),
+                new DelayBehavior8(),
+                new ThrowExceptionBehavior()
+            };
+
+            var context = new BehaviorContext();
+            var chain = new BehaviorChain(behaviors);
+            return chain.Invoke(context);
+        }
+    }
+
+    public class BehaviorChain
+    {
+        private readonly List<IBehavior> behaviors;
+
+        public BehaviorChain(IEnumerable<IBehavior> behaviors)
+        {
+            this.behaviors = behaviors.ToList();
+        }
+
+        public Task Invoke(BehaviorContext context)
+        {
+            return InvokeNext(context, 0);
+        }
+
+        Task InvokeNext(BehaviorContext context, int currentIndex)
+        {
+            if (currentIndex == behaviors.Count)
+            {
+                return Task.CompletedTask;
+            }
+
+            var behavior = behaviors[currentIndex];
+
+            return behavior.Invoke(context, newContext => InvokeNext(newContext, currentIndex + 1));
+        }
+    }
+
+    public interface IBehavior
+    {
+        Task Invoke(BehaviorContext context, Func<BehaviorContext, Task> next);
+    }
+
+    public class DelayBehavior1 : IBehavior
+    {
+        public async Task Invoke(BehaviorContext context, Func<BehaviorContext, Task> next)
+        {
+            await Task.Delay(10).ConfigureAwait(false);
+
+            await next(context).ConfigureAwait(false);
+        }
+    }
+
+    public class DelayBehavior2 : IBehavior
+    {
+        public async Task Invoke(BehaviorContext context, Func<BehaviorContext, Task> next)
+        {
+            await Task.Delay(10).ConfigureAwait(false);
+
+            await next(context).ConfigureAwait(false);
+        }
+    }
+
+    public class DelayBehavior3 : IBehavior
+    {
+        public async Task Invoke(BehaviorContext context, Func<BehaviorContext, Task> next)
+        {
+            await Task.Delay(10).ConfigureAwait(false);
+
+            await next(context);
+        }
+    }
+
+    public class DelayBehavior4 : IBehavior
+    {
+        public async Task Invoke(BehaviorContext context, Func<BehaviorContext, Task> next)
+        {
+            await Task.Delay(10).ConfigureAwait(false);
+
+            await next(context).ConfigureAwait(false);
+        }
+    }
+
+    public class DelayBehavior5 : IBehavior
+    {
+        public async Task Invoke(BehaviorContext context, Func<BehaviorContext, Task> next)
+        {
+            await Task.Delay(10).ConfigureAwait(false);
+
+            await next(context).ConfigureAwait(false);
+        }
+    }
+
+    public class DelayBehavior6 : IBehavior
+    {
+        public async Task Invoke(BehaviorContext context, Func<BehaviorContext, Task> next)
+        {
+            await Task.Delay(10).ConfigureAwait(false);
+
+            await next(context).ConfigureAwait(false);
+        }
+    }
+
+    public class DelayBehavior7 : IBehavior
+    {
+        public async Task Invoke(BehaviorContext context, Func<BehaviorContext, Task> next)
+        {
+            await Task.Delay(10).ConfigureAwait(false);
+
+            await next(context).ConfigureAwait(false);
+        }
+    }
+
+    public class DelayBehavior8 : IBehavior
+    {
+        public async Task Invoke(BehaviorContext context, Func<BehaviorContext, Task> next)
+        {
+            await Task.Delay(10).ConfigureAwait(false);
+
+            await next(context).ConfigureAwait(false);
+        }
+    }
+
+    public class DelayInUsingBehavior1 : IBehavior
+    {
+        public async Task Invoke(BehaviorContext context, Func<BehaviorContext, Task> next)
+        {
+            using (var scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
+            {
+                await Task.Delay(10).ConfigureAwait(false);
+
+                await next(context).ConfigureAwait(false);
+
+                scope.Complete();
+            }
+        }
+    }
+
+    public class DelayInUsingBehavior2 : IBehavior
+    {
+        public async Task Invoke(BehaviorContext context, Func<BehaviorContext, Task> next)
+        {
+            using (var scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
+            {
+                await Task.Delay(10).ConfigureAwait(false);
+
+                await next(context).ConfigureAwait(false);
+
+                scope.Complete();
+            }
+        }
+    }
+
+    public class DelayTwiceBehavior1 : IBehavior
+    {
+        public async Task Invoke(BehaviorContext context, Func<BehaviorContext, Task> next)
+        {
+            await Task.Delay(10).ConfigureAwait(false);
+
+            await next(context).ConfigureAwait(false);
+
+            await Task.Delay(10).ConfigureAwait(false);
+        }
+    }
+
+    public class DelayTwiceBehavior2 : IBehavior
+    {
+        public async Task Invoke(BehaviorContext context, Func<BehaviorContext, Task> next)
+        {
+            await Task.Delay(10).ConfigureAwait(false);
+
+            await next(context).ConfigureAwait(false);
+
+            await Task.Delay(10).ConfigureAwait(false);
+        }
+    }
+
+    public class DelayTwiceBehavior3 : IBehavior
+    {
+        public async Task Invoke(BehaviorContext context, Func<BehaviorContext, Task> next)
+        {
+            await Task.Delay(10).ConfigureAwait(false);
+
+            await next(context).ConfigureAwait(false);
+
+            await Task.Delay(10).ConfigureAwait(false);
+        }
+    }
+
+    public class DelayTwiceBehavior4 : IBehavior
+    {
+        public async Task Invoke(BehaviorContext context, Func<BehaviorContext, Task> next)
+        {
+            await Task.Delay(10).ConfigureAwait(false);
+
+            await next(context).ConfigureAwait(false);
+
+            await Task.Delay(10).ConfigureAwait(false);
+        }
+    }
+
+    public class ThrowExceptionBehavior : IBehavior
+    {
+        public async Task Invoke(BehaviorContext context, Func<BehaviorContext, Task> next)
+        {
+            await Task.Delay(10).ConfigureAwait(false);
+
+            throw new InvalidOperationException(nameof(ThrowExceptionBehavior));
+        }
+    }
+
+    public class PassThroughBehavior1 : IBehavior
+    {
+        public Task Invoke(BehaviorContext context, Func<BehaviorContext, Task> next)
+        {
+            return next(context);
+        }
+    }
+
+    public class PassThroughBehavior2 : IBehavior
+    {
+        public Task Invoke(BehaviorContext context, Func<BehaviorContext, Task> next)
+        {
+            return next(context);
+        }
+    }
+
+    public class BehaviorContext
+    {
+    }
+}
---
+++ AsyncOnly/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AsyncOnly")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AsyncOnly")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a4e80c2d-ebe5-40dc-9977-9540b5d2163a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
